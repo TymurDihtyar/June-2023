3c8ffcf28beff74a38f8b4901143f855b16c0ba9
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-08-15 23:00:17 +0300

    hw1

--- Lesson_1/Lesson1.js
+++ Lesson_1/Lesson1.js
@@ -1,4 +1,43 @@
-console.log(333);
+// =====================================TASK 1================================================== //
+{
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+console.log(`${a} ${b} ${c} ${d} ${e} ${f} ${g} ${h} ${i} ${g} ${k} ${l} ${m}`);
+}
+// =======================================TASK 2================================================ //
 
+let firstName = 'Tymur';
+let middleName = 'Oleksandrovich';
+let lastName = 'Dihtyar';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log (person);
 
-let s = 123;
+// ========================================TASK 3=============================================== //
+
+let a = 100;
+console.log (typeof a);
+let b = '100';
+console.log (typeof b);
+let c = true;
+console.log (typeof c);
+
+// =========================================TASK 4============================================== //
+
+{
+    let firstName = prompt('Enter your firstName');
+    let lastName = prompt('Enter your lastName');
+    let age = prompt('Enter your age');
+    let person = `${firstName} ${lastName} ${age}`;
+    console.log (person);
+}
